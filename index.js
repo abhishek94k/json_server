186ef8f06f09d71f3e02e16b61d99ab07f38a3d8
abhishek94k <113544389+abhishek94k@users.noreply.github.com>
2023-04-20 22:23:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const PORT = process.env.PORT 
+const PORT = process.env.PORT || 3000
 const path = require("path");
 const jsonServer = require("json-server");
 const server = jsonServer.create();
